4b53212a9674a8b9a377f6b05bf95bf0e9c68c04
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2023-01-19 10:08:35 +0300

    control flow

---
+++ bin/control_flow_in_dart.dart
@@ -0,0 +1,5 @@
+import 'package:control_flow_in_dart/control_flow_in_dart.dart' as control_flow_in_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${control_flow_in_dart.calculate()}!');
+}
